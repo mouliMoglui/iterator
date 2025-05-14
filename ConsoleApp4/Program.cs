ed1135b0167e5a933f4cea0a98945c32618d94cb
student <student@S-28-02>
2025-05-14 17:40:24 +0400

    Add project files.

---
+++ ConsoleApp4/Program.cs
@@ -0,0 +1,55 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using System.Runtime.CompilerServices;
+
+//IEnumerable<int> ReturnEven(int n)
+//{
+
+//    for(int i = 0; i < n; i++)
+//    {
+//        if(i % 2 == 0)
+//        {
+//            yield return i;
+//        }
+//    }
+
+//}
+
+
+//var numerable = ReturnNumbers(10);
+
+//IEnumerable<int> ReturnNumbers(int n)
+//{
+//    for(int i= 0; i< n; i++)
+//    { 
+//        yield return i;
+//    }
+//}
+
+//var a = YieldNames();
+
+//foreach(var names in a)
+//{
+//    Console.WriteLine(names);
+//}
+
+//IEnumerable<string> YieldNames()
+//{
+//    yield return "name1";
+//    yield return "name2";
+//    yield return "name3";
+//    yield return "name4";
+
+//}
+
+
+//class MultipleGenerator
+//{
+//    public IEnumerable<int> GetMultiplesOff(int baseNum, int count)
+//    {
+//        for(int i = 0; i < count; i++)
+//        {
+//            yield return count * (i+1);
+//        }
+//    }
+//}
